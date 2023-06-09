c8856a32cfc843809e84d2f0f79b118f02b8a683
Tomas Cerskus <tomas-c@users.noreply.github.com>
2023-06-09 19:31:00 +0100

    Add input for testing state

--- app/test/dashboard/page.tsx
+++ app/test/dashboard/page.tsx
@@ -1,5 +1,10 @@
 "use client";
-
+import { useState  } from "react";
 export default function DashboardPage() {
-  return <div>Page for dashboard</div>;
+  const [text, setText] = useState("");
+  return <div>
+    <div>Page for dashboard</div>
+    <div>Type something to see if this component maintains state between navigation</div>
+    <input type="text" value={text} onChange={(e) => {setText(e.target.value)}}/>
+  </div>;
 }
